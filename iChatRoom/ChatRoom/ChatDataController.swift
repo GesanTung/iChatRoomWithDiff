9ebdc879a54a1b986b1247d11db6de6661c6e4df
老峰 <tungleungs@gmail.com>
2019-06-09 23:34:26 +0800

    upload project

---
+++ iChatRoom/ChatRoom/ChatDataController.swift
@@ -0,0 +1,86 @@
+//
+//  ChatDataController.swift
+//  ChatRoom
+//
+//  Created by Abner on 2019/6/9.
+//  Copyright © 2019 老峰. All rights reserved.
+//
+
+import Foundation
+
+enum ChatSection: CaseIterable {
+    case history, socket
+}
+
+let myUserName = "老峰"
+
+struct ChatMessage: Hashable {
+    let userName: String
+    let msgContent: String
+    let identifier = UUID()
+    
+    var isME: Bool {
+        userName == myUserName
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(identifier)
+    }
+    static func == (lhs: ChatMessage, rhs: ChatMessage) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
+}
+
+class ChatDataController {
+    
+    typealias UpdateHandler = (ChatDataController) -> Void
+    
+    private let updateInterval = 100
+    
+    private let updateHandler: UpdateHandler
+    
+    var displayMsg: [ChatMessage] = [ChatMessage]()
+    
+    init(updateHandler: @escaping UpdateHandler) {
+        self.updateHandler = updateHandler
+        mockRandomMsgUpdate()
+    }
+    
+    private func mockRandomMsgUpdate() {
+        guard allMockMsg.count > 0 else { return }
+        
+        let newIndex = Int.random(in: 0..<allMockMsg.count)
+        
+        let msg = allMockMsg[newIndex]
+        
+        let item = ChatMessage(userName: msg.userName, msgContent: msg.msgContent)
+        
+        displayMsg.append(item)
+        
+        updateHandler(self)
+        
+        let deadline = DispatchTime.now() + DispatchTimeInterval.milliseconds(updateInterval * newIndex)
+        DispatchQueue.main.asyncAfter(deadline: deadline) {
+            self.mockRandomMsgUpdate()
+        }
+    }
+    
+    private var allMockMsg = [ ChatMessage(userName: "老峰", msgContent: "WWDC 2019 好多惊喜"),
+                                ChatMessage(userName: "iOSer", msgContent: "是啊，超棒"),
+                                ChatMessage(userName: "老峰", msgContent: "SWiftUI 也很给力"),
+                                ChatMessage(userName: "iOSer", msgContent: "swift UI也太简单了吧！人人都是开发者"),
+                                ChatMessage(userName: "老峰", msgContent: "说实话，有着全新隐私功能、地图和夜间模式的iOS13实在太赞了"),
+                                ChatMessage(userName: "iOSer", msgContent: "听说 Combine 要取代 RXSwift 了"),
+                                ChatMessage(userName: "老峰", msgContent: "Data Flow Through SwiftUI"),
+                                ChatMessage(userName: "iOSer", msgContent: "What’s New in AppKit for macOS"),
+                                ChatMessage(userName: "老峰", msgContent: "Introducing Combine"),
+                                ChatMessage(userName: "iOSer", msgContent: "Swift 将进入全新时代"),
+                                ChatMessage(userName: "老峰", msgContent: "WWDC 2019 好多惊喜"),
+                                ChatMessage(userName: "iOSer", msgContent: "WWDC 2019 好多惊喜"),
+                                ChatMessage(userName: "老峰", msgContent: "WWDC 2019 好多惊喜"),
+                                ChatMessage(userName: "iOSer", msgContent: "WWDC 2019 好多惊喜"),
+                                ChatMessage(userName: "老峰", msgContent: "WWDC 2019 好多惊喜"),
+                                ChatMessage(userName: "iOSer", msgContent: "WWDC 2019 好多惊喜"),
+                                ChatMessage(userName: "老峰", msgContent: "WWDC 2019 好多惊喜")
+    ]
+}
---
+++ iChatRoom/ChatRoom/ChatViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  ChatRoom
+//
+//  Created by Abner on 2019/6/9.
+//  Copyright © 2019 老峰. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+    let tableView = UITableView(frame: .zero, style: .insetGrouped)
+    var dataSource: UITableViewDiffableDataSource<ChatSection, ChatMessage>?
+    var currentSnapshot = NSDiffableDataSourceSnapshot<ChatSection, ChatMessage>()
+    var chatController: ChatDataController?
+    
+    static let reuseIdentifier = "reuse-identifier"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "ChatRoom"
+        configureTableView()
+        configureDataSource()
+        updateUI()
+    }
+}
+
+extension ChatViewController {
+        
+    func configureTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: ChatViewController.reuseIdentifier)
+    }
+    
+    func configureDataSource() {
+        chatController = ChatDataController { [weak self] (controller: ChatDataController) in
+            guard let self = self else { return }
+            self.updateUI()
+        }
+        
+        self.dataSource = UITableViewDiffableDataSource
+            <ChatSection, ChatMessage>(tableView: tableView) {
+                (tableView: UITableView, indexPath: IndexPath, item: ChatMessage) -> UITableViewCell? in
+                
+                let cell = tableView.dequeueReusableCell(
+                    withIdentifier: ChatViewController.reuseIdentifier,
+                    for: indexPath)
+                let name = item.isME ? item.userName : "\(item.userName)\(indexPath.row)"
+                cell.textLabel?.text = "\(name): \(item.msgContent)"
+                cell.textLabel?.numberOfLines = 0
+                return cell
+        }
+        self.dataSource?.defaultRowAnimation = .fade
+        
+    }
+    
+    func updateUI(animated: Bool = true) {
+        guard let chatcontroller = self.chatController else { return }
+        
+        currentSnapshot = NSDiffableDataSourceSnapshot<ChatSection, ChatMessage>()
+        
+        let items = chatcontroller.displayMsg
+        currentSnapshot.appendSections([.socket])
+        currentSnapshot.appendItems(items, toSection: .socket)
+        
+        self.dataSource?.apply(currentSnapshot, animatingDifferences: animated)
+        self.tableView.scrollToRow(at: IndexPath.init(row: currentSnapshot.numberOfItems(inSection: .socket) - 1, section: 0), at: .bottom, animated: true)
+    }
+
+    
+}
+
+
